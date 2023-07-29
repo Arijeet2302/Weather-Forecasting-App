7045265be336f2e39929434e597e941d760cb96c
Saptarshe Sinha <128472537+Im-Sapto@users.noreply.github.com>
2023-07-29 12:35:36 +0530

    updated

--- src/components/Mapbox.jsx
+++ src/components/Mapbox.jsx
@@ -21,7 +21,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
 
 
-const token = mapboxgl.accessToken = 'process.env.REACT_APP_MAPBOX_API_KEY'
+const token = mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_KEY
 
 
 const Mapbox = ({ location, current }) => {
